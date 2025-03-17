f1ae6840bfab8d26605e6cfc5f4441778417ad96
PJT <swedf123@gmail.com>
2025-03-17 14:52:41 +0900

    ><>

--- src/JT/components/subcomponents/Main.tsx
+++ src/JT/components/subcomponents/Main.tsx
@@ -1,14 +1,29 @@
-type texthd = {
-    text: string,
-    
-}
+type Text = {
+  text: string;
+  img: string;
+};
+
+const data: Text[] = [
+  {
+    text: "Item 1",
+    img: "https://www.twosome.co.kr/resources/images/content/img_p_mobile_illust_.svg",
+  },
+  { text: "Item 2", img: "image2.jpg" },
+  { text: "Item 3", img: "image3.jpg" },
+];
 
 const Main = () => {
-    
+  return (
+    <section>
+      <div>
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>{item.text}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
-    return ( 
-
-     );
-}
- 
 export default Main;
