3b37e650d6a7d2168a739bbdd3701f6dbd78ced3
somi9954 <jolh999@gmail.com>
2025-03-26 17:37:49 +0900

    ..

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ["www.twosome.co.kr"],
   },
+  typescript: {
+    ignoreBuildErrors: true, // 타입스크립트 오류 무시
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ESLint 오류/경고 무시
+  },
 };
 
 module.exports = nextConfig;
